f4a25b29f2f57fe713eebeacbe67d6379dd5b25d
bananafish2828 <bananafish2828@gmail.com>
2017-06-10 15:49:16 -0700

    server open

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 //TODO: fill this page in!
-
+var express = require ('express');
 // require your dependencies
 
+var app = express();
 // create an instance of your express server
-
+var port = 8080;
 
 // depending on which front end framework you're using, 
 // serve your static files
@@ -13,3 +14,6 @@
 // define your routes
 
 // set up your server to listen on your port of choice
+
+app.listen(port);
+console.log('server now listening on port: ', port);
