32febcf4a5cf664c9f6a39c23db8dcc17e84d822
bananafish2828 <bananafish2828@gmail.com>
2017-06-10 20:43:13 -0700

    some changes to movie files

---
+++ react-client/src/components/getMovieButton.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class GetMovieButton extends Component {
+  constructor(props) {
+    super(props)
+  }
+}
+
+
+const Movie = (/*TODO: FILL ME IN*/) => (
+  <div className="single-movie">
+    TODO: FILL ME IN
+  </div>
+)
+
+export default Movie
